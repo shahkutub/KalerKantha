b321df641093297e52647a7a2688a28e8a6672cd
Md. Salahuddin Bhuiyan <salahuddin@aapbd.com>
2016-08-30 12:59:51 +0600

    commiteed 28/8/16

--- app/src/main/java/com/kalerkantho/Fragment/MotamotFragment.java
+++ app/src/main/java/com/kalerkantho/Fragment/MotamotFragment.java
@@ -1,20 +1,23 @@
 package com.kalerkantho.Fragment;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.kalerkantho.R;
-
-;
 
 
 public class MotamotFragment extends Fragment {
     private Context con;
+    private TextView motamotTitle,motatmotBtn,motamotThanks;
+    private EditText subjectEditText,detailsEditText;
 
 
     @Nullable
@@ -32,7 +35,20 @@ public class MotamotFragment extends Fragment {
 
     private void intiU() {
 
+        final Typeface face_reg = Typeface.createFromAsset(con.getAssets(), "fonts/SolaimanLipi_reg.ttf");
+        final Typeface face_bold = Typeface.createFromAsset(con.getAssets(), "fonts/SolaimanLipi_Bold.ttf");
 
+        motamotTitle = (TextView) getView().findViewById(R.id.motamotTitle);
+        subjectEditText = (EditText) getView().findViewById(R.id.subjectEditText);
+        detailsEditText = (EditText) getView().findViewById(R.id.detailsEditText);
+        motatmotBtn = (TextView) getView().findViewById(R.id.motatmotBtn);
+        motamotThanks = (TextView) getView().findViewById(R.id.motamotThanks);
+
+        motamotTitle.setTypeface(face_bold);
+        subjectEditText.setTypeface(face_reg);
+        detailsEditText.setTypeface(face_reg);
+        motatmotBtn.setTypeface(face_bold);
+        motamotThanks.setTypeface(face_reg);
 
     }
 }
--- app/src/main/java/com/kalerkantho/Fragment/SettingFragment.java
+++ app/src/main/java/com/kalerkantho/Fragment/SettingFragment.java
@@ -1,18 +1,23 @@
 package com.kalerkantho.Fragment;
 
 import android.content.Context;;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.kalerkantho.R;
 
 
 
 public class SettingFragment extends Fragment {
     private Context con;
+    private TextView notificationHeadTx,notificationTitle,notificatonAnnounce,soundHeadText,soundTitle;
+    private TextView vivrationHead,vivrationTitle,privacyHead,privacyTitle,tramHead,tramTitle,helpHead,helpTitle;
 
 
     @Nullable
@@ -29,8 +34,38 @@ public class SettingFragment extends Fragment {
     }
 
     private void intiU() {
+        final Typeface face_reg = Typeface.createFromAsset(con.getAssets(), "fonts/SolaimanLipi_reg.ttf");
+        final Typeface face_bold = Typeface.createFromAsset(con.getAssets(), "fonts/SolaimanLipi_Bold.ttf");
+
+        notificationHeadTx = (TextView) getView().findViewById(R.id.notificationHeadTx);
+        notificationTitle = (TextView) getView().findViewById(R.id.notificationTitle);
+        notificatonAnnounce = (TextView) getView().findViewById(R.id.notificatonAnnounce);
+        soundHeadText = (TextView) getView().findViewById(R.id.soundHeadText);
+        soundTitle = (TextView) getView() .findViewById(R.id.soundTitle);
+        vivrationHead = (TextView) getView().findViewById(R.id.vivrationHead);
+        vivrationTitle = (TextView) getView().findViewById(R.id.vivrationTitle);
+        privacyHead = (TextView) getView().findViewById(R.id.privacyHead);
+        privacyTitle = (TextView) getView().findViewById(R.id.privacyTitle);
+        tramHead = (TextView) getView().findViewById(R.id.tramHead);
+        tramTitle = (TextView) getView().findViewById(R.id.tramTitle);
+        helpHead = (TextView) getView().findViewById(R.id.helpHead);
+        helpTitle = (TextView) getView().findViewById(R.id.helpTitle);
 
 
+
+        notificationHeadTx.setTypeface(face_bold);
+        notificationTitle.setTypeface(face_reg);
+        notificatonAnnounce.setTypeface(face_reg);
+        soundHeadText.setTypeface(face_bold);
+        soundTitle.setTypeface(face_reg);
+        vivrationHead.setTypeface(face_bold);
+        vivrationTitle.setTypeface(face_reg);
+        privacyHead.setTypeface(face_bold);
+        privacyTitle.setTypeface(face_reg);
+        tramHead.setTypeface(face_bold);
+        tramTitle.setTypeface(face_reg);
+        helpHead.setTypeface(face_bold);
+        helpTitle.setTypeface(face_reg);
 
     }
 }
